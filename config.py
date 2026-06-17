3c5084fc6ee73b2b507ec7168fbcb828f947165d
Muhammad Khan <muhammadkhan08561@gmail.com>
2026-06-17 10:51:34 +0500

    adding config file

---
+++ config.py
@@ -0,0 +1,8 @@
+
+VOICES = {
+    "greeter": "e07c00bc-4134-4eae-9ea4-1a55fb45746b",
+    "reservation": "9626c31c-bec5-4cca-baa8-f8ba9e84c8bc",
+    "takeaway": "5ee9feff-1265-424a-9d7f-8e4d431a12c7",
+    "checkout": "a167e0f3-df7e-4d52-a9c3-f949145efdab",
+}
+
